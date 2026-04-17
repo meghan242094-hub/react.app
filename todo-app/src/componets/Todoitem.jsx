f8c6d43ac9f10382fde88d1a8066f59c3fd5339f
Your Name <nageshramagond14@gmail.com>
2026-04-17 14:33:40 +0530

    react.app

---
+++ todo-app/src/componets/Todoitem.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Todoitem(todo, index, deleteTodo){
+    return(
+        <div>
+            <p>{todo}</p>
+            <button onClick={()=>deleteTodo(index)}>delete</button>
+        </div>
+    )
+}
+export default Todoitem 
